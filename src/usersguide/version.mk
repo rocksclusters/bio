ca80d6dca42f0eb771c5015982431f004b1efa4f
Mason Katz <mason.katz@gmail.com>
2011-02-14 22:14:16 +0000

    - Build Env   - ROLLNAME defaults to ROLL   - Fixes for docbook - All userguides build again - All userguides are now NOARCH rpms

--- src/usersguide/version.mk
+++ src/usersguide/version.mk
@@ -1,11 +1,11 @@
-ROLL	= bio
 NAME    = roll-$(ROLL)-usersguide
 RELEASE = 0
+RPM.ARCH = noarch
 
 SUMMARY_COMPATIBLE      = $(VERSION)
 SUMMARY_MAINTAINER      = Rocks Group
 SUMMARY_ARCHITECTURE    = i386, x86_64
 
-ROLL_REQUIRES           = base kernel web-server os bio
+ROLL_REQUIRES           = base kernel web-server os
 ROLL_CONFLICTS          =
 
